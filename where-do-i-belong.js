b65b03c02d3e12a4707a24f6e6fbecebebdcdbae
Marcelle Pernet <mevpernet@gmail.com>
2022-07-10 11:52:53 -0300

    good

--- where-do-i-belong.js
+++ where-do-i-belong.js
@@ -3,23 +3,18 @@ function compareNumbers(a, b) {
 }
   
 function getIndexToIns(arr, num) {
-    if (arr.length == 0) {
-        console.log(0);
-        return; 
-    }
-    let newArr = arr.sort(compareNumbers);
+    arr.sort(compareNumbers);
+
     for (let i = 0; i < arr.length; i++) {
-        if (newArr[i] >= num && newArr.length > 0) {
-        newArr.splice(i, 0, num);
+        if (arr[i] >= num) {
         console.log(i);
         return;
         }
-        else if (num > newArr[newArr.length - 1] && newArr.length > 0) {
-        newArr.push(num);
-        console.log(newArr.lastIndexOf(num));
-        return;
-        }
     }
+    console.log(arr.length);   //se length < num ou se length = 0
+    return;
 }
 
 getIndexToIns([], 1);
+
+//Essa versão, menor, não altera o arr de verdade. 
