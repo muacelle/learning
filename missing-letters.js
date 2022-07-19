74a5c9b22d54fa795960ea3fb3582a7c07a3caf5
Marcelle Pernet <mevpernet@gmail.com>
2022-07-19 15:58:19 -0300

    tá feio mas funcionou

--- missing-letters.js
+++ missing-letters.js
@@ -1,16 +1,26 @@
 function fearNotLetter(str) {
     let list = []
     let ascii;
-    for (let i = 0; i < str.length; i++) {
+    let range = [];
+    for (let i = 0; i < str.length; i++) {                       // define list
         list.push(str.charCodeAt(i));
     }
-   //  if ()   // range == list[0]-list[-1]
-    for (let j = 0; j < list.length; j++) {
-        if (list[j] !== list[j-1] + 1) {
-            ascii = list[j-1] + 1
-        }
+    for (let a = list[0]; a <= list[list.length - 1]; a++) {     // define range
+        range.push(a);
     }
-    return String.fromCharCode(ascii);
+    const newRange = [...range].toString();                      // cria as strings pra comparar depois
+    const newList = [...list].toString();
+    if (newRange == newList) {                                   // strings iguais = não falta nenhuma letra
+        return undefined;
+    }
+    else {                                                       // strings diferentes = tem letra faltando
+        for (let j = 0; j < list.length; j++) {                  
+            if (list[j] !== list[j-1] + 1) {
+                ascii = list[j-1] + 1                            // pega o número correspondente à letra faltando
+            }
+        }
+        return String.fromCharCode(ascii);                       // retorna a letra 
+    }
 }
 
 console.log(fearNotLetter("ABCE"));
