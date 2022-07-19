e6f604753c7c3eaecb84666e523ef162215a0167
Marcelle Pernet <mevpernet@gmail.com>
2022-07-19 17:51:25 -0300

    intermediate scripting

---
+++ sorted-union.js
@@ -0,0 +1,6 @@
+function uniteUnique() {
+    return [...arguments].flat().filter((item, index, arr) => arr.indexOf(item) === index);
+}
+  
+
+console.log(uniteUnique([1, 3, 2, 3], [5, 2, 1, 4], [2, 1]));
