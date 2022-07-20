70fd48753dcb6bf2a02319c6aee38235bb577a5e
Marcelle Pernet <mevpernet@gmail.com>
2022-07-20 13:21:07 -0300

    final

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,10 +1,9 @@
 function palindrome(str) {
-    const newStr = str.split('').filter(a => a.match(/[a-zA-Z0-9]/)).toString('').toLowerCase().replace(/,/g, '');
-    return newStr;
+    const newArr = str.split('').filter(a => a.match(/[a-zA-Z0-9]/)).toString('').toLowerCase().split(',');
+    let reverseArr = [...newArr].reverse();
+    const result = newArr.length === reverseArr.length 
+    && newArr.every((value, index) => value === reverseArr[index]) ? 'true' : 'false';
+    return result;
 }
   
-console.log(palindrome("0_0 (: /-\ :) 0-0"));
-
-// filtrar letras e números OK
-// string lowercase
-// 
+console.log(palindrome("race Car"));
