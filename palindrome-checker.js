0c2d4953556e78c686a3b46a431207a491e33faa
Marcelle Pernet <mevpernet@gmail.com>
2022-07-20 13:23:21 -0300

    final de verdade

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -2,7 +2,7 @@ function palindrome(str) {
     const newArr = str.split('').filter(a => a.match(/[a-zA-Z0-9]/)).toString('').toLowerCase().split(',');
     let reverseArr = [...newArr].reverse();
     const result = newArr.length === reverseArr.length 
-    && newArr.every((value, index) => value === reverseArr[index]) ? 'true' : 'false';
+    && newArr.every((value, index) => value === reverseArr[index]) ? true : false;
     return result;
 }
   
