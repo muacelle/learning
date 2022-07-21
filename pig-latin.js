6de85dd9e33c1a9f7a58543c494062c0ffa2e10b
Marcelle Pernet <mevpernet@gmail.com>
2022-07-21 18:02:25 -0300

    FCC Projects

--- pig-latin.js
+++ pig-latin.js
@@ -1,17 +1,12 @@
 function translatePigLatin(str) {
     const vowels = /[aeiou]/g
-    let translated = ''
-    if (vowels.test(str[0])) {          // começa com vogal
-        
-        translated = str + 'way';
 
-    } else {                            // começa com consoante(s)
-        
-        let begin = str.match(/^[bcdfghjklmnpqrstvwxyz]+/g).toString();
-        translated = str.replace(begin, '') + begin + 'ay';
-
+    if (vowels.test(str[0])) {
+        return str + 'way';
     }
-    return translated;
+
+    let begin = str.match(/^[bcdfghjklmnpqrstvwxyz]+/g).toString();
+    return str.replace(begin, '') + begin + 'ay';
 }
   
 console.log(translatePigLatin("algorithm"));
