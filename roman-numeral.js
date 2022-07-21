cd08214ca0f73be8c394d1861346c0cba6360807
Marcelle Pernet <mevpernet@gmail.com>
2022-07-21 11:41:01 -0300

    perfeito menor

--- roman-numeral.js
+++ roman-numeral.js
@@ -15,32 +15,26 @@ const romanNumbersList = {
 }
 
 function getNumberByPosition(number) {
-    let numberSequence = number.toString().split('');
-    console.log(numberSequence)                                // [ '2', '1', '8' ]
-    let multiplier = Math.pow(10, numberSequence.length - 1);
-    console.log(multiplier)                                    // 100
+    let numberSequence = number.toString().split('');          // [ '2', '1', '8' ]                              
+    let multiplier = Math.pow(10, numberSequence.length - 1);  // 100                                 
     
     return numberSequence.map((elem) => {
         const numberByPosition = {
         amount: elem,
         positionHouse: multiplier
         };
-        multiplier /= 10;
-        console.log(numberByPosition)                          // { amount: '2', positionHouse: 100 }, { amount: '1', positionHouse: 10 }, { amount: '8', positionHouse: 1 }
-        return numberByPosition;              
+        multiplier /= 10;                        
+        return numberByPosition;              // { amount: '2', positionHouse: 100 }, { amount: '1', positionHouse: 10 }, { amount: '8', positionHouse: 1 }
     });
 }
   
 function getRomanNumber(position, amount) {
     let number = romanNumbersList[amount * position];          // 200, 10, 4
-    if (number) {    
-        console.log(number);                                          
+    if (number) {                                      
         return number;                                         // 10: X
     } else if (amount < 5) {
-        console.log(number)
         return romanNumbersList[position].repeat(amount);      // 200: 100 (C) -> repeat -> 100 (C) = CC
     }
-    console.log(number)
     return romanNumbersList[5 * position] + romanNumbersList[position].repeat(amount % 5);     // (5 * 1 = 5) + I repeat 3 = VIII
 }
   
