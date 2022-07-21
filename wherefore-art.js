c6bf2f48a5e146853ae5d763a7653d179bd3418d
Marcelle Pernet <mevpernet@gmail.com>
2022-07-21 20:39:43 -0300

    final

--- wherefore-art.js
+++ wherefore-art.js
@@ -1,14 +1,17 @@
 function whatIsInAName(collection, source) {
-    const keys = Object.keys(source); // ['apple', 'bat']
+    const keys = Object.keys(source);
 
     return collection.filter((obj) => {
-        for (let prop in keys) {
-            if (obj.hasOwnProperty(prop) && obj[prop] == source[prop]) {
-                return true;
+        for (let prop of keys) {
+            if (!obj.hasOwnProperty(prop) || obj[prop] !== source[prop]) {
+                console.log(prop)
+                console.log(obj)
+                return false;
             }
         }
+        return true;
     });
 }
     
-console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], 
-{ "apple": 1, "bat": 2 }));
+console.log(whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], 
+{ "apple": 1, "cookie": 2 }));
