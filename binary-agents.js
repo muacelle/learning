3bbd7e9b5970af5c401ebece2dbce69d8e084b20
Marcelle Pernet <mevpernet@gmail.com>
2022-07-24 09:43:42 -0300

    FCC Projects

---
+++ binary-agents.js
@@ -0,0 +1,11 @@
+function binaryAgent(str) {
+    const arr = str.split(' ');
+    let ascii = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        ascii.push(String.fromCharCode(parseInt(arr[i], 2)));
+    }
+    return ascii.join('');
+}
+
+console.log(binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
