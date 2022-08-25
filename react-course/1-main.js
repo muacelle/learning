66aaa02b587cb7cd86d6188f754cdedf326ee3a0
Marcelle Pernet <mevpernet@gmail.com>
2022-08-25 13:15:23 -0300

    first steps

---
+++ react-course/1-main.js
@@ -0,0 +1,31 @@
+
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+// ReactDOM.render(<MainContent/>, document.getElementById("root"))
+
+const element = (
+    <div>
+        <h1 className="header">This is JSX</h1>
+        <p>This is a paragraph</p>
+    </div>
+)
+
+// ReactDOM.render(element, document.getElementById("root"))
+
+const navbar = (
+    <nav>
+        <h1>My Website</h1>
+        <ul>
+            <li>Pricing</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("root"))
+
