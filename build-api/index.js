c30272e81990b1931e13fff7cdb62b2b76e08bc8
Marcelle Pernet <mevpernet@gmail.com>
2022-08-31 15:06:28 -0300

    comentários

--- build-api/index.js
+++ build-api/index.js
@@ -4,7 +4,7 @@ const PORT = 8080;
 
 app.use(express.json()) // Middleware que converte tudo o que chega do body
 
-app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`))
+app.listen(PORT, () => console.log(`It's alive on http://localhost:${PORT}`)) 
 
 app.get('/tshirt', (req, res) => {
     res.status(200).send({
@@ -15,13 +15,15 @@ app.get('/tshirt', (req, res) => {
 
 app.post('/tshirt/:id', (req, res) => {
     const { id } = req.params;
-    const { logo } = req.body;
+    const { logo } = req.body;        // usando Insomnia pra visualizar
 
     if (!logo) {
-        res.status(418).send({ message: 'We need a logo!' })
+        res.status(418).send({ message: 'We need a logo!' })   // se não tiver nada no body
     }
 
     res.send({
-        tshirt: `White tshirt with your ${logo} and ID of ${id}`
+        tshirt: `White tshirt with your ${logo} and ID of ${id}` 
     })
 })
+
+// node . para rodar o servidor na porta 8080
